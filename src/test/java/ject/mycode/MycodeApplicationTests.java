c362450a29f1faf3a39ffb57f119b3ad7e38564c
김도욱 <kdw0737@naver.com>
2025-06-29 23:23:15 +0900

    Init 파일 세팅

---
+++ src/test/java/ject/mycode/MycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ject.mycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
