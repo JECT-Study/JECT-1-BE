3d8b98ac41ffebdef10ddb5238d63523c03764a3
[Kkimdoyeon] <ehdus5176@gmail.com>
2025-09-15 23:02:58 +0900

    [FEAT] CORS 추가

--- src/main/java/ject/mycode/global/config/WebConfig.java
+++ src/main/java/ject/mycode/global/config/WebConfig.java
@@ -25,7 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081", "http://localhost:8082", "https://mycode-ruddy.vercel.app")
+                .allowedOrigins("http://localhost:8081",
+                        "http://localhost:8082",
+                        "https://mycode-ruddy.vercel.app",
+                        "https://mycodemycode.site")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
