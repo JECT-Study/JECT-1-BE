6f63786aefe902776ff7ab34a994294fca1658f0
[Kkimdoyeon] <ehdus5176@gmail.com>
2025-11-23 15:23:55 +0900

    feat: address를 구, 군까지만 가공해서 보냄

--- src/main/java/ject/mycode/domain/content/dto/ContentRegionRes.java
+++ src/main/java/ject/mycode/domain/content/dto/ContentRegionRes.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class ContentRegionRes {
     private Long contentId;
@@ -14,6 +13,38 @@ public class ContentRegionRes {
     private String address;
     private String startDate;
     private String endDate;
+
+    public ContentRegionRes(Long contentId,
+                            String title,
+                            String image,
+                            String address,
+                            String startDate,
+                            String endDate) {
+
+        this.contentId = contentId;
+        this.title = title;
+        this.image = image;
+
+        // 주소는 여기서 가공!
+        this.address = extractGu(address);
+
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    private String extractGu(String address) {
+        String[] parts = address.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (String part : parts) {
+            sb.append(part).append(" ");
+            if (part.endsWith("구") || part.endsWith("군")) {
+                break;
+            }
+        }
+
+        return sb.toString().trim();
+    }
 }
 
 
